b49c5561f1cf92816f276a881b047bddf03eb0d9
Emanuel Castelo <eliaslvcastelo@gmail.com>
2021-09-11 15:33:42 -0300

    Remoção de estrutura desnecessária

--- src/masterproblem.jl
+++ src/masterproblem.jl
@@ -5,12 +5,6 @@ mutable struct MasterProblemData
     V::Vector{Vector{Float64}}
 end
 
-mutable struct MasterProblem
-    model::Model
-    λ::Vector{ConstraintRef}
-    λ_0::ConstraintRef
-end
-
 mutable struct MasterProblemDual
     model::Model
     λ::Vector{VariableRef}
@@ -43,43 +37,4 @@ function build_dualmasterproblem(data::MasterProblemData)::MasterProblemDual
 
     return MasterProblemDual(model, variable_vector, λ_0)
 
-end
-
-function build_masterproblem(data::MasterProblemData)::MasterProblem
-    model = Model(CPLEX.Optimizer, bridge_constraints=false)
-    set_silent(model)
-
-    p = length(data.V)
-
-    c_0 = data.c[1:2]
-
-    A_0 = data.A[:,1:2]
-    A = data.A[:,3:end]
-    m, _ = size(A)
-
-    @variables(model, begin
-        z >= 0
-        0 <= w <= 1
-        α[i=1:p] >= 0
-    end)
-    
-    constraint_vector = Vector{AffExpr}()
-    objective_exp = c_0' * [z, w]
-
-    @inbounds for i = 1:m
-        constraint_exp = A_0[i,:]' * [z, w]
-
-        @inbounds for j = 1:p
-            add_to_expression!(constraint_exp, (A[i,:]' * data.V[j]) * α[j])
-        end
-
-        push!(constraint_vector, constraint_exp)
-    end
-
-    @constraint(model, λ, constraint_vector .>= data.b)
-    @constraint(model, λ_0, sum(α) == 1)
-
-    @objective(model, Min, objective_exp)
-
-    return MasterProblem(model, λ, λ_0)
 end
